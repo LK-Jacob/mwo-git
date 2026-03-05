f76aa34c6eaccb2ee4ab4fac2d2bd9f8f5986a1a
Jakub Malec <Jakub.malec@edu.zs1.wroc.pl>
2026-03-05 14:12:22 +0100

    aktualizacja pliku

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,5 @@
 public class SomeProgram {
     public static void main(String[] args) {
-        System.out.println("Uczymy się używać Git-a.");
+        System.out.println("Będziemy expertami w Git-a");
     }
 }
